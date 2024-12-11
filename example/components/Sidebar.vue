a9bc071802661d47f17d4615f7fd02ea07607532
Rahul-Khera-Codes <rahul.excel2011@gmail.com>
2024-12-11 11:25:23 +0100

    addressed some minor adjustments to files

---
+++ example/components/Sidebar.vue
@@ -0,0 +1,27 @@
+<template>
+  <aside class="menu">
+    <ul class="menu-list">
+      <li>
+        <NuxtLink to="/">Optimized</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/webp">WebP</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/inline">Inline</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/lqi-placeholders">LQI placeholders</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/sqip-placeholders">SQIP placeholders</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/responsive">Responsive</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/avif">AVIF (no optimization)</NuxtLink>
+      </li>
+    </ul>
+  </aside>
+</template>
