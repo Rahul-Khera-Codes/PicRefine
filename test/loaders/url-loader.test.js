6652b65e9a8bc14f90003623714ee8eaf5711291
Rahul-Khera-Codes <rahul.excel2011@gmail.com>
2024-12-11 03:05:21 +0100

    refined project structure and updated files

---
+++ test/loaders/url-loader.test.js
@@ -0,0 +1,20 @@
+const { defaultConfig } = require('../../lib/config')
+const { getUrlLoaderOptions } = require('../../lib/loaders/url-loader')
+
+describe('@aceforth/nuxt-optimized-images/loaders/url-loader', () => {
+  it('uses the default config', () => {
+    const config = Object.assign({}, defaultConfig)
+    const options = getUrlLoaderOptions(config)
+
+    expect(options.limit).toEqual(1000)
+    expect(options.fallback).toEqual('file-loader')
+    expect(options.name).toEqual('img/[contenthash:7].[ext]')
+  })
+
+  it('allows overwriting the inlineImageLimit option', () => {
+    const config = Object.assign(defaultConfig, { inlineImageLimit: 10 })
+    const options = getUrlLoaderOptions(config)
+
+    expect(options.limit).toEqual(10)
+  })
+})
